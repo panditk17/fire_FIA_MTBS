299a41bd759a9d063f371b10e598beae6b99d5a6
Karun Pandit <karunpandit@gmail.com>
2025-01-26 17:14:41 -0500

    Update 1D_plots_western_all_new_RF_6_20.R

--- 1D_plots_western_all_new_RF_6_20.R
+++ 1D_plots_western_all_new_RF_6_20.R
@@ -1,4 +1,5 @@
-### plots based on disturbances from FIA data
+### plots based on disturbances from FIA data2-2-2023
+## codes modified 
 
 # uses only western plots 
 setwd('C:/Karuns_documents/fire_MTBS/all_disturbance/disturbance')
